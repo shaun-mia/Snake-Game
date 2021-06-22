020389ce9a2ba64665c43cca7159772aa3ea38d8
shaun Khan <84870531+shaun-khan@users.noreply.github.com>
2021-06-22 15:54:54 +0600

    Add files via upload

---
+++ snake/GameFrame.java
@@ -0,0 +1,20 @@
+package snake;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+			
+		this.add(new GamePanel());
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+
+	}
+}
